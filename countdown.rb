c817385835886e7f77b7110adff4ad92fa07271a
Miguel Angel Morales <mmorales21500@live.com>
2021-02-04 14:46:55 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
-
-def countdown
+# Loops
+def countdown(num)
+    while num > 0 #num = 10
+        puts "#{num} Second(S)"
+        num -= 1 #decrement 1 and assigns new num => 9
+        sleep(1)
+    end
+    "HAPPY NEW YEAR!" # implicit return
 end
+
+def countdown_with_sleep(num)
+    while num > 0 #num = 10
+        puts "#{num} SECOND(S)!"
+        num -= 1 # decrement 1 and assigns new num => 9
+    end
+    "HAPPY NEW YEAR!" # implicit return
+    sleep(1)
+end
+
